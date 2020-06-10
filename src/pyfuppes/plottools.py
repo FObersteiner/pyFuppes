e4736ececdc4c5c52eee6c385690ae94bb3dc5b4
Florian Obersteiner <florian.obersteiner@kit.edu>
2020-06-10 12:13:18 +0200

    revised get_plot_range function to use numpy methods

--- src/pyfuppes/plottools.py
+++ src/pyfuppes/plottools.py
@@ -11,7 +11,8 @@ import numpy as np
 ###############################################################################
 
 
-def get_plot_range(v, add_percent=5, v_min_lim=False, v_max_lim=False,
+def get_plot_range(v, add_percent=5,
+                   v_min_lim=False, v_max_lim=False,
                    xrange=False, x=False):
     """
     Adjust y-axis range of matplotlib pyplot for a given vector v.
@@ -37,33 +38,30 @@ def get_plot_range(v, add_percent=5, v_min_lim=False, v_max_lim=False,
         lower and upper limit.
 
     """
+    v = np.array(v, dtype=np.float) # ensure array type
 
     if x and xrange:
-        x = np.array(sorted(x)) # monotonically increasing x-vector (e.g. time)
+        x = np.sort(np.array(x)) # monotonically increasing x-vector (e.g. time)
         if len(x) == len(v):
-            w_xvd = np.where((x >= xrange[0]) & (x <= xrange[1]))
+            w_xvd = ((x >= xrange[0]) & (x <= xrange[1]))
             v = v[w_xvd]
 
-    v = list(filter(lambda x: x is not None, v))
-    v = np.array(v)[np.isfinite(np.array(v))]
+    v = v[np.isfinite(v)]
 
     if len(v) < 2:
-        result = None
-    else:
-        result = [1, 1]
-        v_min = min(v)
-        v_max = max(v)
-        offset = (abs(v_min)+abs(v_max))/2 * add_percent/100
-        result[0] = v_min - offset
-        result[1] = v_max + offset
+        raise ValueError("input must at least contain 2 elements")
 
-        if v_min_lim is not False:
-            if result[0] < v_min_lim:
-                result[0] = v_min_lim
+    v_min, v_max = v.min(), v.max()
+    offset = (abs(v_min)+abs(v_max))/2 * add_percent/100
+    result = [v_min - offset, v_max + offset]
 
-        if v_max_lim is not False:
-            if result[1] > v_max_lim:
-                result[1] = v_max_lim
+    if v_min_lim:
+        if result[0] < v_min_lim:
+            result[0] = v_min_lim
+
+    if v_max_lim:
+        if result[1] > v_max_lim:
+            result[1] = v_max_lim
 
     return result
 
