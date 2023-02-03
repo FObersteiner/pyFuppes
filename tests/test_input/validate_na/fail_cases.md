5e58408dd8a36f7c483bfb3f91bc4ed1c66892d0
FObersteiner <florian.obersteiner@kit.edu>
2023-02-03 19:46:13 +0100

    try again

---
+++ tests/test_input/validate_na/fail_cases.md
@@ -0,0 +1,10 @@
+`OM_20200304_591_CPT_MUC_V01_invalid0.txt` - last line only has independent variable
+`OM_20200304_591_CPT_MUC_V01_invalid1.txt` - no data
+`OM_20200304_591_CPT_MUC_V01_invalid2.txt` - invalid nlhead
+`OM_20200304_591_CPT_MUC_V01_invalid3.txt` - invalid ffi
+`OM_20200304_591_CPT_MUC_V01_invalid4.txt` - invalid rdate
+`OM_20200304_591_CPT_MUC_V01_invalid5.txt` - invalid NV
+`OM_20200304_591_CPT_MUC_V01_invalid6.txt` - invalid number of VMISS elements
+`OM_20200304_591_CPT_MUC_V01_invalid7.txt` - invalid number of VNAME elements
+`OM_20200304_591_CPT_MUC_V01_invalid8.txt` - invalid number of nlhead
+`OM_20200304_591_CPT_MUC_V01_invalid9.txt` - non-ascii character
--- tests/test_na1001.py
+++ tests/test_na1001.py
@@ -5,7 +5,12 @@ from pathlib import Path
 
 from pyfuppes.na1001 import FFI1001 as na1001
 
-wd = Path(__file__).parent
+try:
+    wd = Path(__file__).parent
+except NameError:
+    wd = Path.cwd()
+assert wd.is_dir(), "faild to obtain working directory"
+
 src, dst = wd / "test_input", wd / "test_output"
 
 
@@ -13,7 +18,7 @@ class TestNa1001(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # to run before all tests
-        print("\ntesting na1001...")
+        pass
 
     @classmethod
     def tearDownClass(cls):
@@ -29,16 +34,31 @@ class TestNa1001(unittest.TestCase):
         pass
 
     def test_read_valid(self):
-        file = src / "valid_1001a.na"
+        file = src / "validate_na/valid_1001a.na"
         na = na1001(file, sep_data=" ", rmv_repeated_seps=True)
         self.assertIsNotNone(na)
         self.assertEqual(na.NV, 2)
         self.assertEqual(na.NLHEAD, 36)
 
     def test_invalid_read_config(self):
-        file = src / "valid_1001a.na"
+        file = src / "validate_na/valid_1001a.na"
         with self.assertRaises(Exception):
             na1001(file, rmv_repeated_seps=False)
+
+    def test_validate(self):
+        # cases
+        # (filepath, expect_fail?)
+        cases = sorted(
+            (f, "invalid" in f.name) for f in (src / "validate_na").glob("*.txt")
+        )
+        self.assertNotEqual([], cases)
+        for path, expect_fail in cases:
+            if expect_fail:
+                with self.assertRaises(Exception):
+                    _ = na1001(path)
+            else:
+                result = na1001(path)
+                self.assertIsNotNone(result)
 
 
 if __name__ == "__main__":
